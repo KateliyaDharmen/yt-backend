1ca74c5ff4f88d823d8ab3672f1ff3efb77b5e0f
KateliyaDharmen <kateliyadharmen333@gmail.com>
2024-08-21 11:09:13 +0530

    utils, creating a server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 // require('dotenv').config({path: './env'})
 import dotenv from 'dotenv'
 import connectDb from './db/index.js'
+import { app } from './app.js'
 
 dotenv.config({
     path: './env'
 })
 
-connectDb();
+connectDb()
+.then(() => {
+    app.on('Error', (error) => {
+        console.log("Server failed !!", error);
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("MOGODB connection failed !!", error);
+})
 
 
 
